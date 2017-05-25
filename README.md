3586012cc1d3cae36ec553aa977e374568b453a6
Straut, Andrei (Coriant - PT/Amadora) <andrei.straut@coriant.com>
2017-05-26 00:32:07 +0100

    Re-add files

---
+++ README.md
@@ -0,0 +1,12 @@
+# [Start Bootstrap WAR](http://startbootstrap.com/) - [Freelancer](http://startbootstrap.com/template-overviews/freelancer/)
+
+## Overview
+Repackaging of the original Start Bootstrap Freelancer theme into Java WAR file.
+All original assets and functionality are present, only ported to Java.
+
+[Original project location](https://github.com/BlackrockDigital/startbootstrap-freelancer)
+
+## Configure email settings
+Navigate to `src/main/resources/com/andreistraut/freelancerwar/sbfw/config.properties`
+Edit the settings
+Enjoy!
---
+++ src/main/java/com/andreistraut/freelancerwar/sbfw/ContactServlet.java
@@ -0,0 +1,91 @@
+package com.andreistraut.freelancerwar.sbfw;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author straut
+ */
+public class ContactServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	
+	response.setContentType("text/plain;charset=UTF-8");
+	
+	PrintWriter out = response.getWriter();
+	try {
+	    Properties contactProperties = new Properties();
+	    contactProperties.load(this.getClass().getResourceAsStream(
+		    "/com/andreistraut/freelancerwar/sbfw/config/config.properties"));
+	    
+	    String host = request.getParameter("host");
+	    String requestName = request.getParameter("name");
+	    String requestPhone = request.getParameter("phone");
+	    String requestEmail = request.getParameter("email");
+	    String requestMessage = request.getParameter("message");
+	    String mailSubject = String.format(contactProperties.getProperty("emailSubject"), requestName);
+	    String mailBody = String.format(contactProperties.getProperty("emailBody"), 
+		    requestName, requestEmail, requestPhone, requestMessage);
+	    String from = "no-reply@startbootstrapfreelancer.com";
+	    
+	    Properties mailProperties = new Properties();
+	    mailProperties.setProperty("mail.smtp.host", host);
+	    Session session = Session.getDefaultInstance(mailProperties, null);
+
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(from, "NoReply"));
+            msg.addRecipient(Message.RecipientType.TO,
+                             new InternetAddress(contactProperties.getProperty("to"), "Mr. Recipient"));
+            msg.setSubject(mailSubject);
+            msg.setText(mailBody);
+            Transport.send(msg);
+	    
+	    response.setStatus(200);
+	} catch (IOException | MessagingException e) {
+	    Logger.getLogger(com.andreistraut.freelancerwar.sbfw.ContactServlet.class.getName())
+		    .log(Level.SEVERE, "Error occured", e);
+	    out.println(String.format("An unknown exception has occurred: %s", e.getMessage()));
+	    response.setStatus(500);
+	}
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	processRequest(request, response);
+    }
+
+}
